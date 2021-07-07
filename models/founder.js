bb995bb30ee5b58d1a8a842b1abd3006d4dc2853
Agnes Juliana <agnesjuliana26@gmail.com>
2021-07-07 21:46:07 +0700

    name_founder models

--- models/founder.js
+++ models/founder.js
@@ -18,7 +18,7 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.INTEGER,
       primaryKey: true
     },
-    name_founder: DataTypes.STRING,
+    name: DataTypes.STRING,
     email: DataTypes.STRING,
     password: DataTypes.STRING,
     telp: DataTypes.STRING,
